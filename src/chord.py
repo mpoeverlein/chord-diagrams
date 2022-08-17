d01103ada80e3c0d7c53600254652d4de5941643
mpoeverlein <max.poeverlein@gmail.com>
2022-08-17 21:49:45 +0200

    add function to highlight interaction of one unit

--- src/chord.py
+++ src/chord.py
@@ -17,45 +17,67 @@ class Chord:
     def make_arcs(self, arc_lengths, gap_angle, linewidth=3, colors=None):
         colors = self.get_colors(arc_lengths, colors)
         total_lengths = arc_lengths.sum()
-        print(total_lengths)
         self.angle_centers = []
+        self.arcs = []
         for c, (arc_length, color) in enumerate(zip(arc_lengths, colors)):
             start_angle = arc_lengths[:c].sum() / total_lengths * 360 + gap_angle
             end_angle = arc_lengths[:c+1].sum() / total_lengths * 360
-            print(start_angle, end_angle)
             self.angle_centers.append(0.5 * (start_angle + end_angle))
-            self.ax.add_patch(Arc((0, 0), 2, 2, theta1=start_angle, theta2=end_angle, fill=False, lw=linewidth, color=color))
-
-        print(arc_lengths/total_lengths)
-        print(self.angle_centers)
+            arc = Arc((0, 0), 2, 2, theta1=start_angle, theta2=end_angle, fill=False, lw=linewidth, color=color)
+            self.arcs.append(arc)
+            self.ax.add_patch(arc)
 
     def make_labels_horizontal(self, labels, colors=None):
+        self.labels = []
         colors = self.get_colors(labels, colors)
         for label, angle_center, color in zip(labels, self.angle_centers, colors):
             (ha, va, rotation_angle) = self.angle2alignment(angle_center)
-            self.ax.text(*self.angle2coord(angle_center, r=1.1), label, fontsize=12, ha=ha, va=va, color=color)
+            self.labels.append(self.ax.text(*self.angle2coord(angle_center, r=1.1), label, fontsize=12, ha=ha, va=va, color=color))
 
     def make_labels_adjusted(self, labels, colors=None):
+        self.labels = []
         colors = self.get_colors(labels, colors)
         for label, angle_center, color in zip(labels, self.angle_centers, colors):
             (ha, va, rotation_angle) = self.angle2alignment(angle_center)
-            self.ax.text(*self.angle2coord(angle_center, r=1.1), label, fontsize=12, ha=ha, va=va, color=color, rotation=rotation_angle)
+            self.labels.append(self.ax.text(*self.angle2coord(angle_center, r=1.1), label, fontsize=12, ha=ha, va=va, color=color, rotation=rotation_angle))
 
-    def make_connections_from_center(self, interactions):
+    def make_chords_from_centers_of_arcs(self, interactions, symmetric=True):
+        print(interactions)
+        if symmetric:
+            interactions = np.triu(interactions)
+
         for start_index, end_index in np.ndindex(interactions.shape):
             if start_index == end_index: continue
 
             start_angle = self.angle_centers[start_index]
             end_angle = self.angle_centers[end_index]
-
             interaction = interactions[start_index, end_index]
+
+            if interaction == 0: continue
 
             pp1 = PathPatch(
                 Path([self.angle2coord(start_angle), (0,0), self.angle2coord(end_angle)],
                      [Path.MOVETO, Path.CURVE3, Path.CURVE3]),
-                fc="none", transform=self.ax.transData, lw=self.map_correlation(interaction), color=self.map_correlation_color(interaction))
+                     fc="none", 
+                     transform=self.ax.transData, 
+                     lw=self.map_correlation(interaction), 
+                     color=self.map_correlation_color(interaction))
 
             self.ax.add_patch(pp1)
+
+    def make_chord_from_one_unit(self, interactions, unit=0):
+        old_interactions = interactions.copy()
+        interactions = np.zeros_like(interactions)
+        interactions[unit,:] = old_interactions[unit,:]
+        self.make_chords_from_centers_of_arcs(interactions, symmetric=False)
+
+        for arc in self.arcs:
+            arc.set(alpha=0.1)
+        self.arcs[unit].set(alpha=1)
+
+#        for label in self.labels:
+#            label.set(alpha=0.1)
+#        self.labels[unit].set(alpha=1)
     
 
     def show(self):
@@ -77,15 +99,16 @@ class Chord:
 
     @staticmethod
     def get_colors(property_list, colors):
-        if colors is None:
-            colors = 'k'
+        #if colors is None:
+        #    colors = 'k'
+        colors = colors or 'k'
         if isinstance(colors, str):
             colors = len(property_list) * [colors]
 
         return colors
 
     @staticmethod
-    def map_correlation(interaction, threshold=0.5):
+    def map_correlation(interaction, threshold=0):
         return interaction if abs(interaction) >= threshold else 0       
 
     @staticmethod
@@ -96,17 +119,11 @@ class Chord:
 N = 5
 chord = Chord()
 chord.make_arcs(np.random.random(N), 5, colors=['b', 'r', 'b', 'r', 'k'])
-chord.make_labels_horizontal(['long text','long text','clong text','dlong text','elong text'])
-#chord.make_labels_adjusted(['long text','long text','clong text','dlong text','elong text'])
-chord.make_connections_from_center(np.random.random(size=(N,N)))
+#chord.make_labels_horizontal(['along text','blong text','clong text','dlong text','elong text'])
+chord.make_labels_adjusted(['along text','blong text','clong text','dlong text','elong text'])
+#chord.make_connections_from_center(np.random.random(size=(N,N)))
+chord.make_chord_from_one_unit(np.random.random(size=(N,N)), unit=1)
 
 
 chord.show()
 
-
-# pp1 = PathPatch(
-#     Path([angle2coord(subunit_angle1), (0,0), angle2coord(subunit_angle2)],
-#          [Path.MOVETO, Path.CURVE3, Path.CURVE3]),
-#     fc="none", transform=ax.transData, lw=map_correlation(cvalue, threshold=threshold), color=map_correlation_color(cvalue)) 
-#
-# ax.add_patch(pp1)
