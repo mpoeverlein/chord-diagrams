024717b37c5af6a13dd52116fe28e46d25d61153
mpoeverlein <max.poeverlein@gmail.com>
2022-10-04 21:17:21 +0200

    add chords for pokemon types

--- examples/plot_pokemontypes.py
+++ examples/plot_pokemontypes.py
@@ -3,21 +3,29 @@ import pandas as pd
 import chord
 from matplotlib import cm
 
+
+def map_interaction_function(value):
+    return 0 if value == 1 else 1
+
+def map_interaction_color_function(value):
+    d = {0: 'black', 0.5: 'red', 1: 'white', 2: 'blue'} 
+    return d[value]
+
 df = pd.read_csv('pokemontypes.csv', index_col=0, skiprows=1)
 df = df.astype(float)
 
-
-#factor_one = (~(df.notnull())).astype('int')
-#print(factor_one)
-#df += factor_one
-
-print(df)
-
 arc_lengths = np.array([1 for _ in df.columns])
 pkmn_types = df.columns
+type_colors = ['#aaaa99', '#ff4422', '#3399ff', '#ffcc33', '#77cc55', '#66ccff', '#bb5544', '#aa5599', '#ddbb55', '#8899ff', 
+               '#ff5599', '#aabb22', '#bbaa66', '#6666bb', '#7766ee', '#775544', '#aaaabb', '#ee99ee'     
+        ]
+
+props = dict(boxstyle='round', facecolor='white', alpha=0.5)
 
 chord = chord.Chord()
 chord.make_arcs(arc_lengths)
-chord.make_labels(pkmn_types)
+chord.make_labels(pkmn_types, colors=type_colors, text_kwargs={'fontsize': 12, 'bbox': props})
+
+chord.make_chords_from_centers_of_arcs(interactions=df.values, map_interaction_function=map_interaction_function, map_interaction_color_function=map_interaction_color_function, direction='forward')
 
 chord.show()
